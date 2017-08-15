b3be21158262417eded7149ddd550583ff9c3785
jakedub <jacob.w.moore@gmail.com>
2017-08-14 20:34:02 -0400

    add in some for 5 and 2

--- main.js
+++ main.js
@@ -9,21 +9,24 @@ let search = document.getElementsByClassName("search");
 let search_form = document.getElementsByClassName("search-form");
 let results = document.getElementsByClassName("results");
 let inputElement = document.getElementById("myInput");
+let searchInput = document.getElementById("search_button");
+let music = document.getElementById("music");
 // 2. Create your `submit` event for getting the user's search term
 
 //Event Listener for the KeyPress
 
-inputElement.addEventListener("keypress", function(event) {
+inputElement.addEventListener("keypress", function(event) { // NOTE: Need to change the event listener to Submit button click
   if (event.key == "Enter") {
     dataPull();
   }
 });
 
+searchInput.addEventListener("click", dataPull);
+
 // 3. Create your `fetch` request that is called after a submission.
 
-function dataPull(query) {
-    let api = "https://itunes.apple.com/search?term=jack+johnson&limit=25";
-  fetch(api)
+function dataPull() {
+  fetch("https://itunes.apple.com/search?term=")
       .then(function(response) {
           if (response.status !== 200) {
             console.log(response.status);
@@ -32,9 +35,9 @@ function dataPull(query) {
 
          //successful fetch function
           response.json().then(function(data){
-            let returnResponse = document.createElement("div");
-            let inputElement = inputElement.value;
             console.log(data);
+            let returnResponse = document.createElement("div");
+            let input2 = inputElement.value;
             for (let i=0; i<data.length; i++){
               if (inputElement===data[i].artistName){
                 console.log(data[i].artistName);
@@ -47,7 +50,7 @@ function dataPull(query) {
                 <p><span>Album Price: </span>${data[i].collectionPrice}</p>
                 </div>
                 `;
-                container.appendChild(returnResponse);
+                results.appendChild(returnResponse);
             }}
             console.log(data);
           });
@@ -62,14 +65,26 @@ function dataPull(query) {
 
 
 // 4. Create a way to append the fetch results to your page
-
+// NOTE: Looks like I'm doing this within the fetch request. Could put this in another function like:
+// function attached(songs){ //would need to update 3. and replace the below with a call of the function attached like: attached(pulledData) where pulledData is the data.results
+//   let returnResponse = document.createElement("div");
+//   let inputElement = inputElement.value;
+//   for (let i=0; i<songs.length; i++){
+//     if (inputElement===songs[i].artistName){
+//       returnResponse.innerHTML = `
+//       <div class="box">
+//       <img src=${artWorkUrl30} alt="Album Image">
+//       <p><span>Artist Name:</span>${data[i].artistName}</p>
+//       <p><span>Track: </span>${data[i].trackName}</p>
+//       <p><span>Album Name: </span>${data[i].collectionName}</p>
+//       <p><span>Album Price: </span>${data[i].collectionPrice}</p>
+//       </div>
+//       `;
+//       results.appendChild(returnResponse);
+// }
 // 5. Create a way to listen for a click that will play the song in the audio play
 
-
-
-
-
-//set event listener to trigger search only after 'enter' is pressed.
-
-
-//fetch
+function play(){
+  music.addEventListener("click"); //sets the event listener
+  music.setAttribute("src")//need to make the source dynamic
+}
