7d93c7c4569b6b287391721d04b237833e333f69
jakedub <jacob.w.moore@gmail.com>
2017-08-15 17:27:33 -0400

    returns results, needs cleaned up, and audio

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let player = document.getElementsByClassName("player");
 let music_player = document.getElementsByClassName("music-player");
 let search = document.getElementsByClassName("search");
 let search_form = document.getElementsByClassName("search-form");
-let results = document.getElementsByClassName("results");
+let results = document.querySelector(".results");
 let inputElement = document.getElementById("myInput");
 let searchInput = document.getElementById("search_button");
 let music = document.getElementById("music");
@@ -17,7 +17,8 @@ let music = document.getElementById("music");
 
 inputElement.addEventListener("keypress", function(event) { // NOTE: Need to change the event listener to Submit button click
   if (event.key == "Enter") {
-    dataPull();
+    event.preventDefault();
+    dataPull(inputElement.value );
   }
 });
 
@@ -25,8 +26,8 @@ searchInput.addEventListener("click", dataPull);
 
 // 3. Create your `fetch` request that is called after a submission.
 
-function dataPull() {
-  fetch("https://itunes.apple.com/search?term=")
+function dataPull(search) {
+  fetch("https://itunes.apple.com/search?limit=15&term=" + search)
       .then(function(response) {
           if (response.status !== 200) {
             console.log(response.status);
@@ -35,24 +36,7 @@ function dataPull() {
 
          //successful fetch function
           response.json().then(function(data){
-            console.log(data);
-            let returnResponse = document.createElement("div");
-            let input2 = inputElement.value;
-            for (let i=0; i<data.length; i++){
-              if (inputElement===data[i].artistName){
-                console.log(data[i].artistName);
-                returnResponse.innerHTML = `
-                <div class="box">
-                <img src=${artWorkUrl30} alt="Album Image">
-                <p><span>Artist Name:</span>${data[i].artistName}</p>
-                <p><span>Track: </span>${data[i].trackName}</p>
-                <p><span>Album Name: </span>${data[i].collectionName}</p>
-                <p><span>Album Price: </span>${data[i].collectionPrice}</p>
-                </div>
-                `;
-                results.appendChild(returnResponse);
-            }}
-            console.log(data);
+            attached(data.results);
           });
         }
       )
@@ -66,22 +50,23 @@ function dataPull() {
 
 // 4. Create a way to append the fetch results to your page
 // NOTE: Looks like I'm doing this within the fetch request. Could put this in another function like:
-// function attached(songs){ //would need to update 3. and replace the below with a call of the function attached like: attached(pulledData) where pulledData is the data.results
-//   let returnResponse = document.createElement("div");
-//   let inputElement = inputElement.value;
-//   for (let i=0; i<songs.length; i++){
-//     if (inputElement===songs[i].artistName){
-//       returnResponse.innerHTML = `
-//       <div class="box">
-//       <img src=${artWorkUrl30} alt="Album Image">
-//       <p><span>Artist Name:</span>${data[i].artistName}</p>
-//       <p><span>Track: </span>${data[i].trackName}</p>
-//       <p><span>Album Name: </span>${data[i].collectionName}</p>
-//       <p><span>Album Price: </span>${data[i].collectionPrice}</p>
-//       </div>
-//       `;
-//       results.appendChild(returnResponse);
-// }
+function attached(songs){ //would need to update 3. and replace the below with a call of the function attached like: attached(pulledData) where pulledData is the data.results
+  console.log(songs.length);
+  results.innerHTML = "";
+  for (let i=0; i<songs.length; i++){
+      let returnResponse = document.createElement("div");
+      returnResponse.innerHTML = `
+      <div class="box">
+      <img src=${songs[i].artworkUrl30} alt="Album Image">
+      <p><span>Artist Name:</span>${songs[i].artistName}</p>
+      <p><span>Track: </span>${songs[i].trackName}</p>
+      <p><span>Album Name: </span>${songs[i].collectionName}</p>
+      <p><span>Album Price: </span>${songs[i].collectionPrice}</p>
+      </div>
+      `;
+      results.appendChild(returnResponse);
+}
+}
 // 5. Create a way to listen for a click that will play the song in the audio play
 
 function play(){
