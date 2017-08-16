6c3c4147f328ad2ce27e0afbb32b9ca4e92822b4
jakedub <jacob.w.moore@gmail.com>
2017-08-15 21:04:19 -0400

    needs some fine tuning

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ let results = document.querySelector(".results");
 let inputElement = document.getElementById("myInput");
 let searchInput = document.getElementById("search_button");
 let music = document.getElementById("music");
+let preview = [];
 // 2. Create your `submit` event for getting the user's search term
 
 //Event Listener for the KeyPress
@@ -51,25 +52,28 @@ function dataPull(search) {
 // 4. Create a way to append the fetch results to your page
 // NOTE: Looks like I'm doing this within the fetch request. Could put this in another function like:
 function attached(songs){ //would need to update 3. and replace the below with a call of the function attached like: attached(pulledData) where pulledData is the data.results
-  console.log(songs.length);
+preview = [];
   results.innerHTML = "";
   for (let i=0; i<songs.length; i++){
       let returnResponse = document.createElement("div");
+      returnResponse.setAttribute('onclick', `play(${i})`);
       returnResponse.innerHTML = `
       <div class="box">
-      <img src=${songs[i].artworkUrl30} alt="Album Image">
-      <p><span>Artist Name:</span>${songs[i].artistName}</p>
-      <p><span>Track: </span>${songs[i].trackName}</p>
-      <p><span>Album Name: </span>${songs[i].collectionName}</p>
-      <p><span>Album Price: </span>${songs[i].collectionPrice}</p>
+      <img src=${songs[i].artworkUrl60} alt="Album Image">
+      <div class="interior">
+      ${songs[i].trackName} <br/>
+      ${songs[i].artistName}
+      </div>
       </div>
       `;
       results.appendChild(returnResponse);
+      preview.push(songs[i].previewUrl);
 }
 }
 // 5. Create a way to listen for a click that will play the song in the audio play
 
-function play(){
-  music.addEventListener("click"); //sets the event listener
-  music.setAttribute("src")//need to make the source dynamic
+function play(i){
+  console.log(preview[i]);
+  audio = document.getElementById("music");
+  music.setAttribute('src', preview[i]);
 }
